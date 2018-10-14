692422b6377f3354b974dac48ff411f0183f1858
Eugene <zkvasov07@gmail.com>
2018-10-14 16:19:52 +0300

    Initial Code

---
+++ TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            const double PI = 3.1415;
+
+            Console.WriteLine(a * PI);
+        }
+    }
+}
