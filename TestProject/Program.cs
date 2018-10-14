1e93d18caf5226c3f3654d94965a21094d497c4e
Eugene <zkvasov07@gmail.com>
2018-10-14 16:26:44 +0300

    Math update

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -8,12 +8,19 @@ namespace TestProject
 {
     class Program
     {
+        const double PI = 3.1415;
+
         static void Main(string[] args)
         {
-            int a = 5;
-            const double PI = 3.1415;
+            int a = 2;
+            double tmp = PI;
 
-            Console.WriteLine(a * PI);
+            for (int i = 1; i < a; i++)
+            {
+                tmp *= PI;
+            }
+
+            Console.WriteLine(tmp);
         }
     }
 }
